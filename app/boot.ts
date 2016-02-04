7e8e3fa69f28c775003249f48f8b2fa7ce438465
David Hainzl <david@dhainzl.at>
2016-02-05 00:29:09 +0100

    Added routing

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,11 @@
 import { bootstrap } from 'angular2/platform/browser';
+import { provide } from 'angular2/core';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF, ROUTER_PROVIDERS} from 'angular2/router';
+
 import { MainView } from './components/components';
 
-bootstrap (MainView);
+bootstrap(MainView, [
+	ROUTER_PROVIDERS,
+	provide(LocationStrategy, { useClass: HashLocationStrategy }),
+	provide(APP_BASE_HREF, { useValue: '/' })
+]);
--- app/components/MainView/MainView.ts
+++ app/components/MainView/MainView.ts
@@ -1,7 +1,50 @@
-import { Component } from 'angular2/core';
+import { Component, View } from 'angular2/core';
+import { RouteConfig, RouterLink, RouterOutlet, Router } from 'angular2/router';
+
+@Component({
+  
+})
+@View({
+  template: `<h1>Hello from a public route</h1>`
+})
+class PublicRoute {
+	constructor() {
+		console.log('Starting Public Route');
+	}
+}
+
+@Component({
+  
+})
+@View({
+  template: `<h1>Hello from private route</h1>`
+})
+class PrivateRoute {
+	constructor () {
+		console.log('Starting Private Route');
+	}
+}
 
 @Component({
 	selector: 'hockeywatch',
+	directives: [ RouterLink, RouterOutlet ],
 	templateUrl: './app/components/MainView/MainView.html'
 })
-export class MainView { }
+@RouteConfig([
+	{ path: '/public-route', component: PublicRoute, as: 'PublicRoute' },
+	{ path: '/private-route', component: PrivateRoute },
+	{ path: '/', redirectTo: [ './PublicRoute' ] }
+])
+export class MainView {
+	hasRoute(route: string) : boolean {
+		return location.hash.substr(1) == route;
+	}
+
+	publicRouteActive() : boolean {
+		return this.hasRoute('/public-route');
+	}
+
+	privateRouteActive() : boolean {
+		return this.hasRoute('/private-route');
+	}
+}
